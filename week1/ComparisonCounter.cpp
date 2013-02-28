1aa02b2c09fea807ca8f43c02cd3c05eec66bf91
Kasper Nielsen <kasper0406@gmail.com>
2013-02-28 12:36:31 +0100

    Comparison counting

---
+++ week1/ComparisonCounter.cpp
@@ -0,0 +1,9 @@
+//
+//  ComparisonCounter.cpp
+//  AlgEngProject1
+//
+//  Created by Kasper Nielsen on 2/26/13.
+//
+//
+
+#include "ComparisonCounter.h"
---
+++ week1/ComparisonCounter.h
@@ -0,0 +1,14 @@
+//
+//  ComparisonCounter.h
+//  AlgEngProject1
+//
+//  Created by Kasper Nielsen on 2/26/13.
+//
+//
+
+#ifndef __AlgEngProject1__ComparisonCounter__
+#define __AlgEngProject1__ComparisonCounter__
+
+#include <iostream>
+
+#endif /* defined(__AlgEngProject1__ComparisonCounter__) */
