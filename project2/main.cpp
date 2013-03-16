8fe2ecd27760e5221f958d9fb0f64d0f662a3c6c
Lasse Espeholt <lasse@espeholt.dk>
2013-03-16 16:25:19 +0100

    Opdateret rapport

--- project2/main.cpp
+++ project2/main.cpp
@@ -39,8 +39,8 @@ int main(int argc, char *argv[]) {
   //test<RN,CN,RN>(cout, 1, 1024, 1024 * 1024 * 1024);
 
   // Test 2 (Z-curve)
-  test<RN,RN,RN>(cout, 1, 1024, 1024 * 1024 * 1024);
-  test<ZR,ZR,ZR>(cout, 1, 1024, 1024 * 1024 * 1024);
+  /*test<RN,RN,RN>(cout, 1, 1024, 1024 * 1024 * 1024);
+  test<ZN,ZN,ZN>(cout, 1, 1024, 1024 * 1024 * 1024);*/
 
   // Test 3 (Recursive)
   /*test<RN,RN,RN>(cout, 1, 1024, 1024 * 1024 * 1024);
