4032bccf7bb230887597ebbeb387218df6c8a98d
Lasse Espeholt <lasse@espeholt.dk>
2013-03-16 10:24:32 +0100

    Opdateret cmake som virker paa mac/win og ikke skriver cache misses ud mv.

--- project2/main.cpp
+++ project2/main.cpp
@@ -5,36 +5,16 @@
 #include "matrixmul.h"
 #include "test.h"
 
-typedef Matrix<RowBased<double>, Naive> M0;
-typedef Matrix<ColumnBased<double>, Naive> M1;
+typedef Matrix<RowBased<double>, Naive> MR;
+typedef Matrix<ColumnBased<double>, Naive> MC;
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
   cout.precision(8);
-  /*{
-    M0 a(2, 3);
-    M1 b(3, 2);
-
-    a(0, 0) = 1;
-    a(0, 1) = 2;
-    a(0, 2) = 3;
-    a(1, 0) = 4;
-    a(1, 1) = 5;
-    a(1, 2) = 6;
-
-    b(0, 0) = 6;
-    b(0, 1) = 5;
-    b(1, 0) = 4;
-    b(1, 1) = 3;
-    b(2, 0) = 2;
-    b(2, 1) = 1.97;
-    cout << "inden *" << endl;
-    M0 c = a.operator*<M1, M0>(b);
-    cout << "main end" << endl;
-  }*/
-  test<M0,M1,M0>(cout, 5, 1024, 1024 * 1024 * 1024);
-  cout << "done" << endl;
+  
+  test<MR,MR,MR>(cout, 1, 1024, 1024 * 1024 * 1024);
+  test<MR,MC,MR>(cout, 1, 1024, 1024 * 1024 * 1024);
 
   return 0;
 }
