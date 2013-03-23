b3cbdf8e55444637cad2c15c825b01883b16c375
Lasse Espeholt <lasse@espeholt.dk>
2013-03-23 13:26:29 +0100

    ...

--- project2/main.cpp
+++ project2/main.cpp
@@ -16,7 +16,7 @@ bool stack_allocate = false;
 #include "matrixmul.h"
 #include "test.h"
 
-const size_t B = 128;
+const size_t B = 4;
 
 typedef Matrix<RowBased<double>, Naive, false> RN;
 typedef Matrix<ColumnBased<double>, Naive, false> CN;
